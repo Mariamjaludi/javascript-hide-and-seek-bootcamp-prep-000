a73bf82dfa570b7e733a9f3cdd2632c6fbccc321
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-17 18:34:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelector(".target");
+}
+
+function increaseRankBy(n){
+  const temp = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+
+  for (let i=0; i < temp.length; i++){
+    temp[i].innerHTML = parseInt(temp[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  var grandNode = document.getElementById('grand-node');
+  var deeperNode =grandNode.children[0];
+  while (deeperNode){
+    grandNode = deeperNode;
+    if (grandNode.children[0] === undefined){
+      return deeperNode;}
+    else
+      { deeperNode = grandNode.children[0]; }
+  }
+}
